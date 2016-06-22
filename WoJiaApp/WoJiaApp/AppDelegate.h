71e5befc181775a3e2ba50ac75f94b88323e7a9d
cricket <bluecricket@126.com>
2016-06-22 15:30:12 +0800

    初始化项目

---
+++ WoJiaApp/WoJiaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WoJiaApp
+//
+//  Created by cricket on 16/6/22.
+//  Copyright © 2016年 cricket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WoJiaApp/WoJiaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WoJiaApp
+//
+//  Created by cricket on 16/6/22.
+//  Copyright © 2016年 cricket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
